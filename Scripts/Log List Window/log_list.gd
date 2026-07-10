28f7258243ec8e662121586027b747aa427e3130
l1chorpe <l1chorpe@protonmail.com>
2026-07-10 16:09:22 +0200

    feat: separated core and other mods

--- Scripts/Log List Window/log_list.gd
+++ Scripts/Log List Window/log_list.gd
@@ -1,14 +1,27 @@
 extends ItemList
 
+signal request_open_log(log: Log)
+signal request_open_with(log: Log)
+
 var _log_list: Array[Log]
 var _last_clicked_log: Log
 
 
 func _ready() -> void:
-    # Limits the vertical size to 80% of the viewport.
-    resized.connect(func(): custom_minimum_size.y = .8 * get_viewport_rect().size.y)
+    resized.connect(func():
+        # Limits the vertical size to 80% of the viewport
+        custom_minimum_size.y = .35 * get_viewport_rect().size.y
+        # Prevents the menu from being too small for the context menu
+        custom_minimum_size.x = 2 * $ContextMenu.size.x
+    )
 
     item_clicked.connect(_item_clicked)
+
+    # Delegates the requests to the parent
+    $ContextMenu.request_open_log.connect(request_open_log.emit.bind(_last_clicked_log))
+    $ContextMenu.request_open_with.connect(request_open_with.emit.bind(_last_clicked_log))
+    # Triggers on double-click and enter on a focused item; also delegated
+    item_activated.connect(func(_ignored): request_open_log.emit(_last_clicked_log))
 
     $ContextMenu.request_folder_path_copy.connect(_copy_log_folder_path)
     $ContextMenu.request_file_path_copy.connect(_copy_log_file_path)
@@ -20,61 +33,13 @@ func _item_clicked(index: int, at_position: Vector2, mouse_button: int) -> void:
     if mouse_button == MouseButton.MOUSE_BUTTON_RIGHT:
         $ContextMenu.display(at_position)
 
-## Updates the internal log list and the ItemList itself.
-func update() -> Error:
-    if not DirAccess.open(Settings.game_folder):
-        return DirAccess.get_open_error()
-    
-    # Avoids repeating the list when refreshing
-    clear()
-    _log_list.clear()
-
-    # Search through all folders
-    for folder in _get_folders():
-        _log_list.append_array(_search_logs_in(folder))
-    
-    # Return an error when no logs have been found
-    if _log_list.is_empty():
-        return ERR_UNAVAILABLE
-
-    # Display filenames
-    for log_file in _log_list:
-        add_item(log_file.filename)
-    
-    return OK
-
-func get_context_menu() -> Control:
-    return $ContextMenu
+func update_with(logs: Array[Log]) -> void:
+    for l in logs:
+        add_item(l.filename)
+    _log_list = logs
 
 func get_last_selected_log() -> Log:
     return _last_clicked_log
-
-#region Folder searching functions
-## Returns the folders that need to be scanned for logs.
-func _get_folders() -> Array[String]:
-    if Settings.full_search:
-        return [Settings.game_folder]
-    return [
-        Settings.game_folder + "/bin/x64/plugins/cyber_engine_tweaks",
-        Settings.game_folder + "/r6/logs",
-        Settings.game_folder + "/red4ext/logs",
-        Settings.game_folder + "/red4ext/plugins",
-    ]
-
-## Recursively searches through folders for logs.
-func _search_logs_in(folder: String) -> Array[Log]:
-    var logs: Array[Log]
-    var dir = DirAccess.open(folder)
-    dir.list_dir_begin()
-    var filename = dir.get_next()
-    while filename != "":
-        if dir.current_is_dir():
-            logs.append_array(_search_logs_in(folder + "/" + filename))
-        elif filename.ends_with(".log"):
-            logs.append(Log.new(folder, filename))
-        filename = dir.get_next()
-    return logs
-#endregion
 
 #region Copy log data operations
 
--- Scripts/Log List Window/log_list_window.gd
+++ Scripts/Log List Window/log_list_window.gd
@@ -6,18 +6,28 @@ signal request_selection_window
 
 const LogViewer := preload("uid://dqfcxhd8ley3n")
 
+## Used to separate core mod logs
+static var _core_mods_regex: Dictionary[String, RegEx] = {
+    "CET": RegEx.create_from_string("cyber_engine_tweaks"),
+    "redscript": RegEx.create_from_string("redscript_r"),
+    "red4ext": RegEx.create_from_string("red4ext"),
+    "ArchiveXL": RegEx.create_from_string("ArchiveXL"),
+    "Codeware": RegEx.create_from_string("Codeware"),
+    "TweakXL": RegEx.create_from_string("TweakXL")
+}
+
+static var _core_logs: Array[Log]
+static var _other_logs: Array[Log]
+
 
 func _ready() -> void:
     $BackButton.pressed.connect(request_selection_window.emit)
     $VBoxContainer/RefreshButton.pressed.connect(display)
 
-    # Triggers during double-clicks and when pressing enter on an item
-    %LogList.item_activated.connect(func(_ignored): _open_log())
-
-    %LogList.get_context_menu().request_open_log.connect(_open_log)
-    %LogList.get_context_menu().request_open_with.connect(_show_open_with_dialog)
-    %LogList.get_context_menu().request_folder_path_copy.connect(_send_folder_path_copied_notif)
-    %LogList.get_context_menu().request_file_path_copy.connect(_send_file_path_copied_notif)
+    %CoreLogList.request_open_log.connect(_open_log)
+    %CoreLogList.request_open_with.connect(_show_open_with_dialog)
+    %OtherLogList.request_open_log.connect(_open_log)
+    %OtherLogList.request_open_with.connect(_show_open_with_dialog)
 
     $OpenWithDialog.file_selected.connect(_open_with)
     $OpenWithDialog.canceled.connect(_open_with_dismissed)
@@ -26,28 +36,121 @@ func _ready() -> void:
 ##
 ## TODO: handle more errors
 func display() -> void:
-    match %LogList.update():
-        OK: 
-            %LogList.show()
-        ERR_UNAVAILABLE:
-            $VBoxContainer/NoLogsLabel.show()
-
+    _update_logs()
+    _push_logs()
     show()
 
-#region Log operations
+## Updates the logs (either when searching or refreshing).
+func _update_logs() -> void:
+    # TODO: handle path not accessible
 
-func _open_log() -> void:
+    _core_logs.clear()
+    _other_logs.clear()
+
+    # Search for logs
+    var logs: Array[Log]
+    for path in _get_folders_paths():
+        logs.append_array(_search_logs_in(path))
+
+    _split_logs(logs)
+
+## Pushes logs into the corresponding lists or shows an error if no logs were found.
+func _push_logs() -> void:
+    if _core_logs.size() != 0:
+        %CoreLogList.update_with(_core_logs)
+        %CLLContainer.show()
+    else:
+        $VBoxContainer/NoCoreLogsLabel.show()
+        %CLLContainer.hide()
+
+    if _other_logs.size() != 0:
+        %OtherLogList.update_with(_other_logs)
+        %OLLContainer.show()
+    else:
+        $VBoxContainer/NoOtherLogsLabel.show()
+        %OLLContainer.hide()
+
+
+
+#region Log opening
+
+func _open_log(log_: Log) -> void:
     # Open externally if set to do so
     if Settings.always_ext_edit:
-        _open_with(Settings.ext_edit_path)
+        _open_with(Settings.ext_edit_path, log_)
         return
-    
+
     # Open with internal editor
-    var last_log: Log = %LogList.get_last_selected_log()
     add_child(LogViewer.instantiate().with(
-            last_log.filename,
-            last_log.full_path
+            log_.filename,
+            log_.full_path
     ))
+
+func _show_open_with_dialog() -> void:
+    $OpenWithDialog.show()
+
+## Opens a log with the selected program and hides the context menu.
+func _open_with(exe_path: String, log_: Log) -> void:
+    OS.create_process(
+            exe_path,
+            [log_.full_path]
+    )
+    %LogList.get_context_menu().hide()
+
+## Prevents the context menu from disappearing if no program was selected.
+func _open_with_dismissed() -> void:
+    %LogList.get_context_menu().grab_focus.call_deferred()
+
+#endregion
+
+
+#region Log searching
+
+## Splits logs into core and other mod logs.
+func _split_logs(logs: Array[Log]) -> void:
+    # Underscore avoids name collision with log()
+    for log_ in logs:
+        if _is_core_mod(log_):
+            _core_logs.push_back(log_)
+        else:
+            _other_logs.push_back(log_)
+
+## Checks if a mod is core or not.
+static func _is_core_mod(log_: Log) -> bool:
+    for mod in _core_mods_regex:
+        if _core_mods_regex[mod].search(log_.filename) != null:
+            return true
+    return false
+
+## Returns the folders to be searched according to the settings.
+static func _get_folders_paths() -> Array[String]:
+    if Settings.full_search:
+        return [Settings.game_folder]
+    return [
+        Settings.game_folder + "/bin/x64/plugins/cyber_engine_tweaks",
+        Settings.game_folder + "/r6/logs",
+        Settings.game_folder + "/red4ext/logs",
+        Settings.game_folder + "/red4ext/plugins",
+    ]
+
+## Searches for the logs in the given folder.
+static func _search_logs_in(folder: String) -> Array[Log]:
+    var logs: Array[Log]
+    var dir = DirAccess.open(folder)
+    dir.list_dir_begin()
+    var filename = dir.get_next()
+    while filename != "":
+        if dir.current_is_dir():
+            logs.append_array(_search_logs_in(folder + "/" + filename))
+        elif filename.ends_with(".log"):
+            logs.append(Log.new(folder, filename))
+        filename = dir.get_next()
+    return logs
+
+#endregion
+
+
+#region notifications
 
 ## Displays a notification when the folder path was copied.
 func _send_folder_path_copied_notif() -> void:
@@ -56,20 +159,5 @@ func _send_folder_path_copied_notif() -> void:
 ## Displays a notification when the file path was copied.
 func _send_file_path_copied_notif() -> void:
     $FilePathCopiedNotification.trigger()
-
-func _show_open_with_dialog() -> void:
-    $OpenWithDialog.show()
-
-## Opens a log with the selected program and hides the context menu.
-func _open_with(exe_path: String) -> void:
-    OS.create_process(
-            exe_path,
-            [%LogList.get_last_selected_log().full_path]
-    )
-    %LogList.get_context_menu().hide()
-
-## Prevents the context menu from disappearing if no program was selected.
-func _open_with_dismissed() -> void:
-    %LogList.get_context_menu().grab_focus.call_deferred()
 
 #endregion
